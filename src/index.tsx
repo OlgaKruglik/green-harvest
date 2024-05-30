88757e0d58989282e4fb8cec5245a43b722b5a92
OlgaKruglik <137288699+OlgaKruglik@users.noreply.github.com>
2024-05-30 12:53:49 +0300

    init: add fixed

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,13 @@ import './style/index.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
+    
 <React.StrictMode>
 <Provider store={store}>
+    
 <App />
 </Provider>
 </React.StrictMode>
@@ -18,3 +21,5 @@ root.render(
 } else {
 console.error('Элемент с ID "root" не найден.');
 }
+
+
